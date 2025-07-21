bb8255a5eebce143f1e6dcbb71a5ce584ca658fa
jngaiii <jngai_@berkeley.edu>
2025-07-21 12:47:05 -0700

    made changes to setup.py but commented out code on initializing sub repositories for debugging

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 import sys
 from setuptools import setup, find_packages
-from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 with open('README.md') as f:
     readme = f.read()
@@ -12,21 +13,39 @@ def update_submodules():
     # Check if the .git folder exists
     if os.path.exists(os.path.join(base_dir, '.git')):
         print("Updating git submodules...")
-        # Run submodule init and update for 'vortex'
-        subprocess.check_call(['git', 'submodule', 'init', 'vortex'], cwd=base_dir)
-        subprocess.check_call(['git', 'submodule', 'update', 'vortex'], cwd=base_dir)
+        # Run submodule init and update for 'evo2' --> it's setup file initializes the submodules it needs
+        subprocess.run(["git", "submodule", "update", "--init", "--recursive", "evo2"], check=True, cwd=base_dir)
     else:
         print("No .git directory found; skipping submodule update.")
 
-class CustomBuild(_build_py):
+def install_all_submodules(root_dir="."):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        if "setup.py" in filenames:
+            # Avoid reinstalling the root project
+            if os.path.abspath(dirpath) == os.path.abspath("."):
+                continue
+            print(f"📦 Installing submodule at: {dirpath}")
+            subprocess.run(["pip", "install", "-e", dirpath], check=True)
+            # Optionally also run its submodules if nested
+            subprocess.run(["git", "submodule", "update", "--init", "--recursive"], cwd=dirpath, check=True)
+
+
+class CustomInstall(install):
     def run(self):
         update_submodules()
-        super().run()
+        install_all_submodules()
+        super().run() # main project installs after installing submodules
+
+class CustomDevelop(develop):
+    def run(self):
+        update_submodules()
+        install_all_submodules()
+        super().run() # main project installs after installing submodules
 
 setup(
     name="phllm",  # e.g., prokbert-pipeline
     version="0.1.2",
-    packages=find_packages(),
+    packages=find_packages(),  # can also fo 'include=["phllm", "phllm.*", "evo2.evo2", "evo2.evo2.*"]'
     install_requires=[
         "numpy<2",
         "pandas",
@@ -40,9 +59,10 @@ setup(
         "biopython",
         "tqdm"
     ],
-    # cmdclass={
-    #     'build': CustomBuild
-    # }, 
+    cmdclass={
+        # 'install': CustomInstall,
+        # 'develop': CustomDevelop
+    }, 
     python_requires=">=3.11", 
     description="Pipeline for genomic language model embeddings, processing and utilization in predicting phage-host interactions",
     long_description=readme, 
